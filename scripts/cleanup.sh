0b11434edf2ba46535d97103e4669592433a8723
MayankTripathi033 <mayank033023@gmail.com>
2025-01-27 09:27:55 +0530

    Added cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-rm -f /home/ec2-user/Todo-Frontend/.gitignore
+
+# Check if the file exists
+if [ -f "/home/ec2-user/Todo-Frontend/.gitignore" ]; then
+    echo "Removing .gitignore file..."
+    sudo rm -f /home/ec2-user/Todo-Frontend/.gitignore
+else
+    echo ".gitignore file not found, skipping removal."
+fi
